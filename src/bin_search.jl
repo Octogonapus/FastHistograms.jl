9302196dc09a74fc7d0b0f36752fa3d63bf49fea
Octogonapus <firey45@gmail.com>
2021-05-29 17:13:21 -0400

    Update Arithmetic docs

--- src/bin_search.jl
+++ src/bin_search.jl
@@ -1,14 +1,21 @@
 "A trait for the ways the bin search step can be implemented."
 abstract type BinSearchAlgorithm end
 
-"Basic arithmetic to determine the bin to update, compatible only with the FixedWidth bin type."
+"""
+Basic arithmetic to determine the bin to update, compatible only with the FixedWidth bin type.
+
+Requires these functions to be defined:
+- `binmin(hist)::Int` Returns the value of the lowest bin edge.
+- `norm(hist)::Float32` Returns the inverse of the size of the bin range (`1 / (last_bin - first_bin)`).
+- `nbins(hist)::Int` Returns the number of bins.
+"""
 struct Arithmetic <: BinSearchAlgorithm end
 
 """
 Uses binary search to find the bin to update. Meant to be used with the VariableWidth bin type.
 
 Requires these functions to be defined:
-`bin_ranges(hist)::Vector{Int}`
+- `bin_ranges(hist)::Vector{Int}`
 """
 struct BinarySearch <: BinSearchAlgorithm end
 
