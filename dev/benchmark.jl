370226f27284e0a079081873db32b577f956cc1c
Octogonapus <firey45@gmail.com>
2021-06-07 18:59:33 -0400

    Clarify benchmarks

--- dev/benchmark.jl
+++ dev/benchmark.jl
@@ -1,58 +1,66 @@
 using FastHistograms, StatsBase, BenchmarkTools, DataFrames, PrettyTables
 
 function bench_noparallel(img1, img2)
-    h_noparallel = create_fast_histogram(
+    h_fw_a_np = create_fast_histogram(
         FastHistograms.FixedWidth(),
         FastHistograms.Arithmetic(),
         FastHistograms.NoParallelization(),
         [(0x00, 0xff, 16), (0x00, 0xff, 16)],
     )
-    hist_bench_noparallel = @benchmarkable increment_bins!($h_noparallel, $img1, $img2)
+    bench_fh_fw_a_np = @benchmarkable increment_bins!($h_fw_a_np, $img1, $img2)
 
-    h_noparallel_bs = create_fast_histogram(
-        FastHistograms.FixedWidth(),
+    edge1 = [0.0, 0.1, 0.5, 0.9, 1.0]
+    edge2 = [0.0, 0.5, 0.6, 1.0]
+    h_vw_bs_np = create_fast_histogram(
+        FastHistograms.VariableWidth(),
         FastHistograms.BinarySearch(),
         FastHistograms.NoParallelization(),
-        [(0x00, 0xff, 16), (0x00, 0xff, 16)],
+        [edge1, edge2],
     )
-    hist_bench_noparallel_bs = @benchmarkable increment_bins!($h_noparallel, $img1, $img2)
+    bench_fh_vw_bs_np = @benchmarkable increment_bins!($h_vw_bs_np, $img1, $img2)
 
-    h_simd = create_fast_histogram(
+    h_fw_a_simd = create_fast_histogram(
         FastHistograms.FixedWidth(),
         FastHistograms.Arithmetic(),
         FastHistograms.SIMD(),
         [(0x00, 0xff, 16), (0x00, 0xff, 16)],
     )
-    hist_bench_simd = @benchmarkable increment_bins!($h_simd, $img1, $img2)
+    bench_fh_fw_a_simd = @benchmarkable increment_bins!($h_fw_a_simd, $img1, $img2)
 
     img1_vec = vec(img1)
     img2_vec = vec(img2)
-    stats_base_bench = @benchmarkable StatsBase.fit(
+    bench_sb_fw = @benchmarkable StatsBase.fit(
         StatsBase.Histogram,
         ($img1_vec, $img2_vec),
         (0x00:UInt8(16):0x10f, 0x00:UInt8(16):0x10f),
     )
 
-    return stats_base_bench, hist_bench_noparallel, hist_bench_noparallel_bs, hist_bench_simd
+    bench_sb_vw = @benchmarkable StatsBase.fit(
+        StatsBase.Histogram,
+        ($img1_vec, $img2_vec),
+        ($edge1, $edge2),
+    )
+
+    return bench_fh_fw_a_np, bench_fh_vw_bs_np, bench_fh_fw_a_simd, bench_sb_fw, bench_sb_vw
 end
 
 function bench_parallel(data)
     img_vec = vec(data)
-    stats_base_bench = @benchmarkable StatsBase.fit(
+    bench_sb_fw = @benchmarkable StatsBase.fit(
         StatsBase.Histogram,
         $img_vec,
         range(Float32(0.0); stop = Float32(512.0), length = 513),
     )
 
-    h_private_threads = create_fast_histogram(
+    h_fw_a_pt = create_fast_histogram(
         FastHistograms.FixedWidth(),
         FastHistograms.Arithmetic(),
         FastHistograms.PrivateThreads(),
         [(Float32(0.0), Float32(512.0), 512)],
     )
-    hist_bench_private_threads = @benchmarkable increment_bins!($h_private_threads, $data)
+    bench_fh_fw_a_pt = @benchmarkable increment_bins!($h_fw_a_pt, $data)
 
-    return stats_base_bench, hist_bench_private_threads
+    return bench_fh_fw_a_pt, bench_sb_fw
 end
 
 bench_df_row(trial) = [time(trial), gctime(trial), allocs(trial), memory(trial)]
@@ -64,13 +72,14 @@ function run_all_benchmarks(img_type, img_size)
     results_noparallel = run.(bench_noparallel(img1, img2))
 
     df = DataFrame(
-        "Rows" => ["Expected Time (ns)", "Min Time (ns)", "GC Time (ns)", "Allocs (B)", "Memory (B)"],
-        "StatsBase" => [32000, bench_df_row(results_noparallel[1])...],
-        "FH (NoParallel)" => [12000, bench_df_row(results_noparallel[2])...],
-        "FH (NoParallel, BS)" => [12000, bench_df_row(results_noparallel[3])...],
-        "FH (SIMD)" => [15000, bench_df_row(results_noparallel[4])...],
+        "Rows" => ["Min Time (ns)", "GC Time (ns)", "Allocs (B)", "Memory (B)"],
+        "FastHistograms:\nFixedWidth,\nArithmetic,\nNoParallelization" => bench_df_row(results_noparallel[1]),
+        "FastHistograms:\nVariableWidth,\nBinarySearch,\nNoParallelization" => bench_df_row(results_noparallel[2]),
+        "FastHistograms:\nFixedWidth,\nArithmetic,\nSIMD" => bench_df_row(results_noparallel[3]),
+        "StatsBase:\nFixedWidth" => bench_df_row(results_noparallel[4]),
+        "StatsBase:\nVariableWidth" => bench_df_row(results_noparallel[5]),
     )
-    pretty_table(df)
+    pretty_table(df, autowrap = true, linebreaks = true)
 end
 
 function run_all_benchmarks()
@@ -87,11 +96,11 @@ function run_all_benchmarks()
     tune!.(bench_parallel(ahtl_data))
     results_parallel = run.(bench_parallel(ahtl_data))
     df = DataFrame(
-        "Rows" => ["Expected Time (ns)", "Min Time (ns)", "GC Time (ns)", "Allocs (B)", "Memory (B)"],
-        "StatsBase" => [0, bench_df_row(results_parallel[1])...],
-        "FH (PrivateThreads, AHTL)" => [108249000, bench_df_row(results_parallel[2])...],
+        "Rows" => ["Min Time (ns)", "GC Time (ns)", "Allocs (B)", "Memory (B)"],
+        "FastHistograms:\nFixedWidth,\nArithmetic,\nPrivateThreads" => bench_df_row(results_parallel[1]), # AHTL is 108249000 ns
+        "StatsBase:\nFixedWidth" => bench_df_row(results_parallel[2]),
     )
-    pretty_table(df)
+    pretty_table(df, autowrap = true, linebreaks = true)
 end
 
 run_all_benchmarks()
